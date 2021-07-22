d0ec3712db15500d19494432cd85afd3f186c530
Muhammad Nurwahid <wahidisdead@gmail.com>
2021-07-22 12:14:12 +0700

    added recoil-persist save theme in localstorage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const theme = atom({
   key: "theme",
   default: "light",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export { theme };
